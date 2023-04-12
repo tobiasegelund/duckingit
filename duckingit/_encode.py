8ddd2753dcb0d8bd48eb7fcb57b67b6153a1a6b4
Tobias Egelund <egelundtobias@gmail.com>
2023-04-12 15:29:54 +0200

    Add format enum

--- duckingit/_encode.py
+++ duckingit/_encode.py
@@ -1,4 +1,15 @@
 import hashlib
+from enum import Enum
+
+
+class Format(Enum):
+    PARQUET = "parquet"
+    JSON = "json"
+
+
+#     CSV = "csv"
+#     ORC = "orc"
+#     AVRO = "avro"
 
 
 def create_md5_hash_string(string: str) -> str:
