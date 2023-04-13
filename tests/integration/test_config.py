ca17fc044f0eb7970b68f8b8b53288b12c8f9963
Tobias Egelund <egelundtobias@gmail.com>
2023-04-13 19:21:56 +0200

    Add integration on config

---
+++ tests/integration/test_config.py
@@ -0,0 +1,12 @@
+from duckingit._config import DuckConfig
+
+
+def test_DuckConfig():
+    got = False
+    try:
+        DuckConfig().memory_size(128).timeout(30).warm_up().update()
+        got = True
+    except Exception:
+        got = False
+
+    assert got
