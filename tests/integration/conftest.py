d690603a6c181bea167751461b1dc5f46095f2e8
Tobias Egelund <egelundtobias@gmail.com>
2023-04-13 19:24:35 +0200

    Update test func name

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,5 +4,10 @@ from duckingit import DuckSession
 
 
 @pytest.fixture
+def test_function_name_aws():
+    yield "TestFunc"
+
+
+@pytest.fixture
 def session():
     yield DuckSession(function_name="DuckExecutor")
--- tests/integration/test_config.py
+++ tests/integration/test_config.py
@@ -1,10 +1,12 @@
 from duckingit._config import DuckConfig
 
 
-def test_DuckConfig():
+def test_DuckConfig(test_function_name_aws):
     got = False
     try:
-        DuckConfig().memory_size(128).timeout(30).warm_up().update()
+        DuckConfig(function_name=test_function_name_aws).memory_size(128).timeout(
+            30
+        ).warm_up().update()
         got = True
     except Exception:
         got = False
