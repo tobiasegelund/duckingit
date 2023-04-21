6bbc45ef791d88b81e33d596c7eb53e0f278f760
Tobias Egelund <egelundtobias@gmail.com>
2023-04-21 14:35:22 +0200

    Correct compatible arch

--- infrastructure/modules/lambda_layer/main.tf
+++ infrastructure/modules/lambda_layer/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_layer_version" "this" {
   filename                 = var.src
   layer_name               = "duckdb"
   description              = "A installation of DuckDB binaries"
-  compatible_architectures = ["x86_64"]
+  compatible_architectures = ["arm64"]
 
   compatible_runtimes = ["python3.9"]
 }
