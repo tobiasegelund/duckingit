b7ef838bfbdddddd9b84d59f733b913c44b15ab2
Tobias Egelund <egelundtobias@gmail.com>
2023-05-17 21:57:38 +0200

    Add integrations module

---
+++ duckingit/integrations/__init__.py
@@ -0,0 +1,6 @@
+"""Integrations with Lakehouse solutions such as Delta Lake, Apache Iceberg and Apache Hudi"""
+
+# class Formats(Enum):
+#     DELTA = "delta"
+#     ICEBERG = "iceberg"
+#     HUDI = "hudi"
